f6ee3f1e3352d8c98440edd7ff57d7cc0f547f9c
rajeshggm <rajesh.manjrekar002@gmail.com>
2018-04-16 13:22:37 +0530

    push v2

--- Login.java
+++ Login.java
@@ -70,7 +70,7 @@ public class Login extends JFrame implements ActionListener{
         if(e.getSource()==buttonok)
         {
            
-           if(id.equals("hello")&&pass.equals("56789"))//id pass change
+           if(id.equals("hello")&&pass.equals("56789"))//id pass change v2
             {
            
         
